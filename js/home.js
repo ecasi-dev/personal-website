801e4a2ea7cf85ccf1f479e4a4b175978528f5df
ecasi-dev <ecasi.dev@gmail.com>
2022-03-18 02:58:20 +0800

    Added sample table

---
+++ js/home.js
@@ -0,0 +1,62 @@
+const homeData = [
+  {
+    title: "Project 1 Sample",
+    date: "January 14, 2022",
+    tags: [
+      {
+        tag: "Engineering",
+        link: "#",
+      },
+      {
+        tag: "Web Development",
+        link: "#",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis neque ultricies consectetur aliquet sapien, venenatis ultrices ornare aliquam.",
+    link: "#",
+  },
+  {
+    title: "Project 2 Sample",
+    date: "January 14, 2022",
+    tags: [
+      {
+        tag: "Engineering",
+        link: "#",
+      },
+      {
+        tag: "Web Development",
+        link: "#",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis neque ultricies consectetur aliquet sapien, venenatis ultrices ornare aliquam.",
+    link: "#",
+  },
+  {
+    title: "Project 3 Sample",
+    date: "January 14, 2022",
+    tags: [
+      {
+        tag: "Engineering",
+        link: "#",
+      },
+      {
+        tag: "Web Development",
+        link: "#",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis neque ultricies consectetur aliquet sapien, venenatis ultrices ornare aliquam.",
+    link: "#",
+  },
+];
+
+
+function buildTable(){
+    const table = document.getElementById('homeTable');
+
+    for(let i = 0; i < homeData.length; i++){
+        let row = ``
+    }
+}
